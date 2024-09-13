25a97b48389b9b528e39c2deb34bd486ffd3ae9c
Alona Maksymova <alonamaksymova5@gmail.com>
2024-09-13 12:32:24 +0300

    create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Description from './components/Description/Description';
+
+const App = () => {
+  return (
+    <div>
+      <Description />
+    </div>
+  )
+}
+
+export default App;
